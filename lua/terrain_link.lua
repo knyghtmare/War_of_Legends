9ad7f6e3a48173ca594a9e16abd46daa4e6e261a
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2021-03-02 12:21:32 +0600

    Create terrain_link.lua

---
+++ lua/terrain_link.lua
@@ -0,0 +1,12 @@
+-- Nightmare faction specific ability
+-- Units with this ability will adapt a new
+-- variation based on the terrain they are on
+-- Current List:
+-- Base - flat/castle/keeps/village
+-- Bramble - Forests
+-- Rock - Mountains/Hills/Caves
+-- SandStorm - Desert/Sand/Dunes
+-- Whirlpool - Water/Deepwater/Fords
+-- Swamp-whirlpool - Swampwater/Quagmire
+
+local T = wml.tag 
