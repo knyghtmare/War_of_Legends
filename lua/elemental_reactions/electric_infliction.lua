e66cffce7ebf417dc9b2402f39a155403705e0c6
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-12-20 10:52:49 +0800

    Create electric_infliction.lua

---
+++ lua/elemental_reactions/electric_infliction.lua
@@ -0,0 +1,15 @@
+function wesnoth.wml_actions.WOL_add_electric_status(cfg)	
+	local units = wesnoth.units.find_on_map(cfg)
+	for _,u in pairs(units) do
+		u:add_modification("object",{
+			id = "WOL_ele_status_object",
+            duration = "turn",
+            -- add overlay
+			T.effect{
+				apply_to="overlay",
+				add="misc/electric-overlay.png"
+			}
+		})
+		u.status.electric_status = true
+	end
+end
