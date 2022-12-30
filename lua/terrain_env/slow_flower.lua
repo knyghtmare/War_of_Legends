b5515b4ddd64386e10e0d339d325314ce1ba5072
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-12-30 17:24:46 +0800

    Create slow_flower.lua

---
+++ lua/terrain_env/slow_flower.lua
@@ -0,0 +1,8 @@
+local x = wesnoth.current.event_context.x1
+local y = wesnoth.current.event_context.y1
+local units = wesnoth.units.find_on_map({x = x, y = y})
+for i,u in ipairs(units) do
+    wesnoth.audio.play( "entangle.wav" )
+    u.status.slowed = true
+    wesnoth.current.map.get(x, y).terrain = wesnoth.current.map.get(x, y).base_terrain
+end
