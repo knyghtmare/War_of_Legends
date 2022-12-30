896b9deab140c29692a59a306176d6ff937a87c3
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-12-30 17:24:31 +0800

    Update cold_infliction.lua

--- lua/elemental_reactions/cold_infliction.lua
+++ lua/elemental_reactions/cold_infliction.lua
@@ -2,7 +2,7 @@ function wesnoth.wml_actions.WOL_add_cold_status(cfg)
 	local units = wesnoth.units.find_on_map(cfg)
 	for _,u in pairs(units) do
 		u:add_modification("object",{
-			id = "WOL_ele_status_object",
+			id = "WOL_cold_status_object",
             duration = "turn",
             -- add overlay
 			T.effect{
