32f36c7ce8ece9326b53dbccd844dbc94b1685ae
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2023-02-10 23:32:40 +0800

    Update help.lua

--- lua/gui/help.lua
+++ lua/gui/help.lua
@@ -1,4 +1,4 @@
---#textdomain wesnoth-NX-RPG
+--#textdomain wesnoth-War_of_Legends
 
 local dialog, data, page_count =
 	nxrequire 'gui/dialogs/help', nxrequire 'data/help'
@@ -9,7 +9,7 @@ local function select_topic()
 	if i > page_count then
 		wesnoth.fire("wml_message", {
 			logger = "error",
-			message = "[NX] BUG: invalid topic_list row number"
+			message = "[WoL] BUG: invalid topic_list row number"
 		})
 
 		return
