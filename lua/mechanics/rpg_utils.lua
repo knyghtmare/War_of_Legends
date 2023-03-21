2899233d60303ecc5ad6eed35b9b9b769db0ced6
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2023-03-21 16:15:17 +0800

    update code

--- lua/mechanics/rpg_utils.lua
+++ lua/mechanics/rpg_utils.lua
@@ -31,16 +31,20 @@ wesnoth.game_events.add_repeating("unit placed", function()
                     id = "allied_leadership",
                 }
             }
-        }
+        },
     }
     -- take the unit as local variable
     local ecx = wesnoth.current.event_context
     local u = wesnoth.units.get(ecx.x1, ecx.y1) or wml.error("[XP balance] No unit found at (x1, y1) to add XP in incrementor")
 
-    if u:ability("leadership") and u.variables.rpg_adjusted then
+    if u:ability("leadership") and not u.variables.rpg_adjusted then
         -- perform the operation
         u:add_modification("object", effects)
+        -- debug message to check it worked or not
+        print("%s at (%d,%d) has been adjusted", u.id, u.x, u.y)
     end
+
+    u.variables.rpg_adjusted = true
 end)
 
 -- Replace Inspires with Allied Inspires
@@ -69,14 +73,18 @@ wesnoth.game_events.add_repeating("unit placed", function()
                     id = "allied_inspires",
                 }
             }
-        }
+        },
     }
     -- take the unit as local variable
     local ecx = wesnoth.current.event_context
     local u = wesnoth.units.get(ecx.x1, ecx.y1) or wml.error("[XP balance] No unit found at (x1, y1) to add XP in incrementor")
 
-    if u:ability("inspire") and u.variables.rpg_adjusted then
+    if u:ability("inspire") and not u.variables.rpg_adjusted then
         -- perform the operation
         u:add_modification("object", effects)
+        -- debug message to check it worked or not
+        print("%s at (%d,%d) has been adjusted", u.id, u.x, u.y)
     end
+
+    u.variables.rpg_adjusted = true
 end)
