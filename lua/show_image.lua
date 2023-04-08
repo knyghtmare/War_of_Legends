90f9aa85cafd3e50c5253ee2704cbd65870d7e10
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2023-04-08 16:17:04 +0800

    Create show_image.lua

---
+++ lua/show_image.lua
@@ -0,0 +1,42 @@
+local T = wml.tag
+
+---
+-- Displays an image on a popup dialog.
+--
+-- [show_image]
+--     image= ...
+-- [/show_image]
+---
+function wesnoth.wml_actions.show_image(cfg)
+	local function do_error(msg)
+		wml.error("[show_image]: " .. msg)
+	end
+
+	local img = cfg.image or do_error("required 'image' attribute is missing")
+
+	local dd = {
+		-- NOTE: Can't use transparency until mainline #19165 is fixed!
+		-- definition = "message",
+		maximum_width = 800,
+		maximum_height = 480,
+		click_dismiss = true,
+		T.helptip { id="tooltip_large" }, -- mandatory field
+		T.tooltip { id="tooltip_large" }, -- mandatory field
+		T.grid {
+			T.row {
+				T.column {
+					border = "all", border_size = 5,
+					T.image {
+						id = "image", definition = "default"
+					}
+				}
+			}
+		}
+	}
+
+	local function preshow()
+		wesnoth.set_dialog_value(img, "image")
+	end
+
+	wesnoth.show_dialog(dd, preshow, nil)
+end
