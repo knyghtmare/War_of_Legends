f055c5b11ec3f8f8f0c2834b86f401deba76be10
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2023-07-05 01:36:44 +0800

    Update level_increment.lua

--- lua/mechanics/level_increment.lua
+++ lua/mechanics/level_increment.lua
@@ -1,3 +1,13 @@
 function wesnoth.effects.level(unit, cfg)
-	unit.level = unit.level + cfg.increase
+	if cfg.set then
+		unit.level = cfg.set
+	elseif cfg.increase then
+		unit.level = unit.level + cfg.increase
+	elseif cfg.new_level then
+		unit.level = cfg.new_level
+	elseif cfg.value then
+		unit.level = cfg.value
+	else
+		wml.error("Invalid or Missing key in [effect] apply_to=level")
+	end
 end
