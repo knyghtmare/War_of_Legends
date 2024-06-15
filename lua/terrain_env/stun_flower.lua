8ef3b91a5890a28003a1a41244fcaa728f826dc7
Tahsin Jahin Khalid <5283677+knyghtmare@users.noreply.github.com>
2024-06-15 20:49:40 +0600

    update to have condition in brackets

--- lua/terrain_env/stun_flower.lua
+++ lua/terrain_env/stun_flower.lua
@@ -3,7 +3,7 @@ local y = wesnoth.current.event_context.y1
 local units = wesnoth.units.find_on_map({x = x, y = y})
 for i,u in ipairs(units) do
     -- stun does not work on level 0 units
-    if u.level != 0 then
+    if (u.level != 0) then
         wesnoth.audio.play( "entangle.wav" )
         u.status.slowed = true
         wesnoth.current.map.get(x, y).terrain = wesnoth.current.map.get(x, y).base_terrain
