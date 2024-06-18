a21457d10fc118d78c7ee3019da94955fd929096
Tahsin Jahin Khalid <5283677+knyghtmare@users.noreply.github.com>
2024-06-18 21:26:48 +0600

    Update stun_flower.lua

--- lua/terrain_env/stun_flower.lua
+++ lua/terrain_env/stun_flower.lua
@@ -1,11 +1,11 @@
-local x = wesnoth.current.event_context.x1
-local y = wesnoth.current.event_context.y1
-local units = wesnoth.units.find_on_map({x = x, y = y})
-for i,u in ipairs(units) do
+-- local x = wesnoth.current.event_context.x1
+-- local y = wesnoth.current.event_context.y1
+-- local units = wesnoth.units.find_on_map({x = x, y = y})
+-- for i,u in ipairs(units) do
     -- stun does not work on level 0 units
-    if (u.level != 0) then
-        wesnoth.audio.play( "entangle.wav" )
-        u.status.slowed = true
-        wesnoth.current.map.get(x, y).terrain = wesnoth.current.map.get(x, y).base_terrain
-    end
-end
+    -- if (u.level != 0) then
+    --     wesnoth.audio.play( "entangle.wav" )
+    --     u.status.slowed = true
+    --     wesnoth.current.map.get(x, y).terrain = wesnoth.current.map.get(x, y).base_terrain
+--     -- end
+-- end
