7520315a846e7031d4c3b70872fcf723079c7787
Tahsin Jahin Khalid <5283677+knyghtmare@users.noreply.github.com>
2024-08-05 13:05:44 +0600

    Update theme.lua

--- lua/theme.lua
+++ lua/theme.lua
@@ -7,13 +7,6 @@ function wesnoth.interface.game_display.unit_status()
 	if not u then return {} end
 	local s = old_unit_status()
 
-	--if u.status.dehydrated then
-	--	table.insert(s, { "element", {
-	--		image = "misc/dehydration-status.png~CROP_TRANSPARENT()",
-	--		tooltip = _ "dehydrated: This unit is dehydrated. It will lose 4 HP and have its damage reduced by 1 each turn during the day unless prevented by healers or cured by water at an oasis.\n\nUnits cannot be killed or deal no damage as a result of dehydration."
-	--	} })
-	--end
-
 	if u.status.dazed then
 		table.insert(s, { "element", { image = "misc/dazed-status-icon.png~CROP_TRANSPARENT()",
 			tooltip = _ "dazed: This unit is dazed. It suffers a -10% penalty to both its defense and chance to hit (except for magical attacks)."
