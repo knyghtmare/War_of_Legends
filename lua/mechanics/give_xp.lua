d81aadd988d2cfda50edae5a8837cd57318865c6
Tahsin Jahin Khalid <5283677+knyghtmare@users.noreply.github.com>
2026-02-13 18:06:32 +0600

    Update give_xp.lua

--- lua/mechanics/give_xp.lua
+++ lua/mechanics/give_xp.lua
@@ -19,7 +19,7 @@ function wesnoth.wml_actions.set_experience(cfg)
     -- get the receiving unit(s)
     for index, unit in ipairs(wesnoth.units.find_on_map(cfg)) do
         if unit.valid then
-            -- wesnoth.interface.float_label( unit.x, unit.y, string.format("<span color='cyan'>+%s XP</span>", tostring(amount) ) )
+            wesnoth.interface.float_label( unit.x, unit.y, string.format("<span color='cyan'>%s XP</span>", tostring(amount) ) )
             unit.experience = amount
         end
     end    
