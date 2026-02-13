005dfd0544ad6bbf55cbe9ed03684c29c095f068
Tahsin Jahin Khalid <5283677+knyghtmare@users.noreply.github.com>
2026-02-13 18:05:07 +0600

    Update give_xp.lua

--- lua/mechanics/give_xp.lua
+++ lua/mechanics/give_xp.lua
@@ -10,3 +10,17 @@ function wesnoth.wml_actions.give_experience(cfg)
         end
     end    
 end
+
+
+function wesnoth.wml_actions.set_experience(cfg)
+    -- get the XP amount
+    local amount = tonumber(cfg.amount) or wml.error("Missing or wrong percentage= value in [set_experience]")
+
+    -- get the receiving unit(s)
+    for index, unit in ipairs(wesnoth.units.find_on_map(cfg)) do
+        if unit.valid then
+            -- wesnoth.interface.float_label( unit.x, unit.y, string.format("<span color='cyan'>+%s XP</span>", tostring(amount) ) )
+            unit.experience = amount
+        end
+    end    
+end
