9834512df50a50dafca79bb08eeb313c6f4b5ef7
Tahsin Jahin Khalid <5283677+knyghtmare@users.noreply.github.com>
2026-03-08 04:06:11 +0600

    Update recall_all.lua

--- lua/mechanics/recall_all.lua
+++ lua/mechanics/recall_all.lua
@@ -5,3 +5,23 @@ function wesnoth.wml_actions.recall_all(cfg)
 		wesnoth.wml_actions.recall({ id = u.id })
 	end
 end
+
+
+function wesnoth.wml_actions.recall_highest(cfg)
+    -- recall highest level unit on x, y
+    local x = cfg.x or wml.error( "Missing or wrong x= value in [recall_highest]" )
+    local y = cfg.y or wml.error( "Missing or wrong y= value in [recall_highest]" )
+
+    for level = 5, 1, -1 do
+        wesnoth.wml_actions.recall({
+            level = level,
+            x = x,
+            y = y,
+        })
+
+        local unit = wesnoth.get_unit(x, y)
+        if unit ~= nil then
+            return
+        end
+    end
+end
