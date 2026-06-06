d64b0e2390d9ec9185716552af96bad265d6b12a
Tahsin Jahin Khalid <5283677+knyghtmare@users.noreply.github.com>
2026-06-06 21:46:26 +0600

    Update recall_cost.lua

--- lua/mechanics/recall_cost.lua
+++ lua/mechanics/recall_cost.lua
@@ -8,9 +8,9 @@ function wesnoth.wml_actions.adjust_recall_costs(cfg)
     -- |     0      |     10      |
     -- |     1      |     15      |
     -- |     2      |     20      |
-    -- |     3      |     30      |
-    -- |     4      |     40      |
-    -- |     5+     |     50      |
+    -- |     3      |     25      |
+    -- |     4      |     30      |
+    -- |     5+     |     35      |
     -- | ---------- | ----------- |
     for _, unit in ipairs(wesnoth.units.find_on_recall {}) do
         if unit.level == 0 then
@@ -20,11 +20,33 @@ function wesnoth.wml_actions.adjust_recall_costs(cfg)
         elseif unit.level == 2 then
             unit.recall_cost = 20
         elseif unit.level == 3 then
-            unit.recall_cost = 30
+            unit.recall_cost = 25
         elseif unit.level == 4 then
-            unit.recall_cost = 40
+            unit.recall_cost = 30
         else 
-            unit.recall_cost = 50
+            unit.recall_cost = 35
+        end
+    end
+end
+
+function wesnoth.wml_actions.adjust_recall_costs_mp(cfg)
+    -- currently revised recall costs
+    -- | Unit Level | Recall Cost |
+    -- | ---------- | ----------- |
+    -- |     0      |     10      |
+    -- |     1      |     15      |
+    -- |     2      |     20      |
+    -- |     3      |     20      |
+    -- |     4      |     20      |
+    -- |     5+     |     20      |
+    -- | ---------- | ----------- |
+    for _, unit in ipairs(wesnoth.units.find_on_recall {}) do
+        if unit.level == 0 then
+            unit.recall_cost = 10
+        elseif unit.level == 1 then
+            unit.recall_cost = 15
+        else 
+            unit.recall_cost = 20
         end
     end
 end
