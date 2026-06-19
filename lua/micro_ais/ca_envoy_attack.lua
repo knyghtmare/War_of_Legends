09123d5ba3de5c00f5d6b4c325764bbd8cfb2230
Tahsin Jahin Khalid <5283677+knyghtmare@users.noreply.github.com>
2026-06-20 03:07:09 +0600

    update micro ai envoy

--- lua/micro_ais/ca_envoy_attack.lua
+++ lua/micro_ais/ca_envoy_attack.lua
@@ -1,32 +1,34 @@
-local AH = wesnoth.require "ai/lua/ai_helper"
+local AH = wesnoth.require "ai/lua/ai_helper.lua"
 
 local messenger_next_waypoint = wesnoth.require "./ca_envoy_next_waypoint"
 local best_attack
 
-local function messenger_find_enemies_in_way(messenger, goal_x, goal_y)
+local function messenger_find_enemies_in_way(messenger, goal_x, goal_y, avoid_map)
     -- Returns the first unit on or next to the path of the messenger
     -- @messenger: proxy table for the messenger unit
     -- @goal_x,@goal_y: coordinates of the goal toward which the messenger moves
     -- Returns proxy table for the first unit found, or nil if none was found
-    
-    local path, cost = AH.find_path_with_shroud(messenger, goal_x, goal_y, { ignore_units = true })
+
+    local path, cost = AH.find_path_with_avoid(messenger, goal_x, goal_y, avoid_map, { ignore_enemies = true })
     if cost >= 42424242 then return end
-    
+
     -- The second path hex is the first that is important for the following analysis
     if (not path[2]) then return end
-    
+
     -- Is there an enemy unit on the second path hex?
     -- This would be caught by the adjacent hex check later, but not in the right order
     local enemy = wesnoth.units.get(path[2][1], path[2][2])
     if AH.is_attackable_enemy(enemy) then return enemy end
-    
+
     -- After that, go through adjacent hexes of all the other path hexes
     for i = 2,#path do
-        local sub_path, sub_cost = AH.find_path_with_shroud(messenger, path[i][1], path[i][2], { ignore_units = true })
+        local sub_path, sub_cost = AH.find_path_with_avoid(messenger, path[i][1], path[i][2], avoid_map, { ignore_enemies = true })
         if (sub_cost <= messenger.moves) then
             for xa,ya in wesnoth.current.map:iter_adjacent(path[i]) do
-                local next_enemy = wesnoth.units.get(xa, ya)
-                if AH.is_attackable_enemy(next_enemy) then return next_enemy end
+                local new_enemy = wesnoth.units.get(xa, ya)
+                if AH.is_attackable_enemy(new_enemy) then
+                    return new_enemy
+                end
             end
         else  -- If we've reached the end of the path for this turn
             return
@@ -40,10 +42,12 @@ local function messenger_find_clearing_attack(messenger, goal_x, goal_y, cfg)
     -- @messenger: proxy table for the messenger unit
     -- @goal_x,@goal_y: coordinates of the goal toward which the messenger moves
     -- Returns proxy table containing the attack, or nil if none was found
-    
-    local enemy_in_way = messenger_find_enemies_in_way(messenger, goal_x, goal_y)
+
+    local avoid_map = AH.get_avoid_map(ai, wml.get_child(cfg, "avoid"), true)
+
+    local enemy_in_way = messenger_find_enemies_in_way(messenger, goal_x, goal_y, avoid_map)
     if (not enemy_in_way) then return end
-    
+
     local filter = wml.get_child(cfg, "filter") or { id = cfg.id }
     local units = AH.get_units_with_attacks {
         side = wesnoth.current.side,
@@ -51,41 +55,44 @@ local function messenger_find_clearing_attack(messenger, goal_x, goal_y, cfg)
         { "and", wml.get_child(cfg, "filter_second") }
     }
     if (not units[1]) then return end
-    
+
     local attacks = AH.get_attacks(units, { simulate_combat = true })
-    
-    local max_rating = -9e99
+
+    local max_rating = - math.huge
     for _,attack in ipairs(attacks) do
-        if (attack.target.x == enemy_in_way.x) and (attack.target.y == enemy_in_way.y) then
-            
+        if (attack.target.x == enemy_in_way.x) and (attack.target.y == enemy_in_way.y)
+            and (not avoid_map:get(attack.dst.x, attack.dst.y))
+        then
             -- Rating: expected HP of attacker and defender
             local rating = attack.att_stats.average_hp - 2 * attack.def_stats.average_hp
-            
+
             if (rating > max_rating) then
                 max_rating, best_attack = rating, attack
             end
         end
     end
-    
+
     if best_attack then return best_attack end
-    
+
     -- If we got here, that means there's an enemy in the way, but none of the units can reach it
     --> try to fight our way to that enemy
     for _,attack in ipairs(attacks) do
         -- Rating: expected HP of attacker and defender
-        local rating = attack.att_stats.average_hp - 2 * attack.def_stats.average_hp
-        
-        -- Give a huge bonus for closeness to enemy_in_way
-        local tmp_defender = wesnoth.units.get(attack.target.x, attack.target.y)
-        local dist = wesnoth.map.distance_between(enemy_in_way, tmp_defender)
-        
-        rating = rating + 100. / dist
-        
-        if (rating > max_rating) then
-            max_rating, best_attack = rating, attack
+        if (not avoid_map:get(attack.dst.x, attack.dst.y)) then
+            local rating = attack.att_stats.average_hp - 2 * attack.def_stats.average_hp
+
+            -- Give a huge bonus for closeness to enemy_in_way
+            local tmp_defender = wesnoth.units.get(attack.target.x, attack.target.y)
+            local dist = wesnoth.map.distance_between(enemy_in_way.x, enemy_in_way.y, tmp_defender.x, tmp_defender.y)
+
+            rating = rating + 100. / dist
+
+            if (rating > max_rating) then
+                max_rating, best_attack = rating, attack
+            end
         end
     end
-    
+
     if best_attack then return best_attack end
 end
 
@@ -93,21 +100,22 @@ local ca_messenger_attack = {}
 
 function ca_messenger_attack:evaluation(cfg)
     -- Attack units in the path of the messengers
-    
+
     local messenger, x, y = messenger_next_waypoint(cfg)
     if (not messenger) then return 0 end
-    
+
     local attack = messenger_find_clearing_attack(messenger, x, y, cfg)
-    
+
     if attack then
         return cfg.ca_score
     end
-    
+
     return 0
 end
 
 function ca_messenger_attack:execution(cfg)
-    AH.robust_move_and_attack(ai, best_attack.src, best_attack.dst, best_attack.target)
+    local avoid_map = AH.get_avoid_map(ai, wml.get_child(cfg, "avoid"), true)
+    AH.robust_move_and_attack(ai, best_attack.src, best_attack.dst, best_attack.target, { avoid_map = avoid_map })
     best_attack = nil
 end
 
--- lua/micro_ais/envoy_mai.lua
+++ lua/micro_ais/envoy_mai.lua
@@ -1,15 +1,22 @@
+local AH = wesnoth.require "ai/lua/ai_helper.lua"
+
 function wesnoth.micro_ais.envoy(cfg)
-	if (cfg.action ~= 'delete') and (not cfg.id) and (not wml.get_child(cfg, "filter")) then
-		wml.error("Envoy [micro_ai] tag requires either id= key or [filter] tag")
+	if (cfg.action ~= 'delete') then
+		if (not cfg.id) and (not wml.get_child(cfg, "filter")) then
+			wml.error("Envoy [micro_ai] tag requires either id= key or [filter] tag")
+		end
+		AH.get_multi_named_locs_xy('waypoint', cfg, 'Envoy [micro_ai] tag')
 	end
-	local required_keys = { waypoint = 'string' }
-	local optional_keys = { id = 'string', enemy_death_chance = 'float', filter = 'tag', filter_second = 'tag', invert_order = 'boolean', messenger_death_chance = 'float' }
+	local required_keys = {}
+	local optional_keys = { avoid = 'tag', id = 'string', enemy_death_chance = 'float', filter = 'tag', filter_second = 'tag',
+		invert_order = 'boolean', messenger_death_chance = 'float', waypoint_loc = 'string', waypoint_x = 'integer_list', waypoint_y = 'integer_list'
+	}
 	local score = cfg.ca_score or 300000
 	local CA_parms = {
 		ai_id = 'mai_envoy',
-		{ ca_id = 'attack', location = '~add-ons/War_of_Legends/lua/micro_ais/ca_envoy_attack.lua', score = score + 2 },
-		{ ca_id = 'move', location = '~add-ons/War_of_Legends/lua/micro_ais/ca_envoy_move.lua', score = score + 10 },
-		{ ca_id = 'escort_move', location = '~add-ons/War_of_Legends/lua/micro_ais/ca_envoy_escort_move.lua', score = score + 1 }
+		{ ca_id = 'attack', location = '~add-ons/War_of_Legends/lua/micro_ais/ca_envoy_attack.lua', score = score },
+		{ ca_id = 'move', location = '~add-ons/War_of_Legends/lua/micro_ais/ca_envoy_move.lua', score = score - 1 },
+		{ ca_id = 'escort_move', location = '~add-ons/War_of_Legends/lua/micro_ais/ca_envoy_escort_move.lua', score = score - 2 }
 	}
-    return required_keys, optional_keys, CA_parms
+	return required_keys, optional_keys, CA_parms
 end
