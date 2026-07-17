ba7eca5263ed982a30f28bc15dc42bba6860d960
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2026-07-17 14:12:30 +0600

    update micro ai envoy (#319)
    Co-authored-by: Tahsin Jahin Khalid <5283677+knyghtmare@users.noreply.github.com>

--- lua/micro_ais/ca_envoy_escort_move.lua
+++ lua/micro_ais/ca_envoy_escort_move.lua
@@ -1,5 +1,6 @@
-local AH = wesnoth.require "ai/lua/ai_helper"
+local AH = wesnoth.require "ai/lua/ai_helper.lua"
 local LS = wesnoth.require "location_set"
+local MAIUV = wesnoth.require "ai/micro_ais/micro_ai_unit_variables.lua"
 local M = wesnoth.map
 
 local messenger_next_waypoint = wesnoth.require "./ca_envoy_next_waypoint"
@@ -18,85 +19,73 @@ function ca_messenger_escort_move:evaluation(cfg)
     -- Move escort units close to messengers, and in between messengers and enemies
     -- The messengers have moved at this time, so we don't need to exclude them,
     -- but we check that there are messengers left
-    
+
     if (not get_escorts(cfg)[1]) then return 0 end
-    
+
     local _, _, _, messengers = messenger_next_waypoint(cfg)
     if (not messengers) or (not messengers[1]) then return 0 end
-    
+
     return cfg.ca_score
 end
 
-function ca_messenger_escort_move:execution(cfg, data)
+function ca_messenger_escort_move:execution(cfg)
     local escorts = get_escorts(cfg)
-    local messenger = messenger_next_waypoint(cfg)
-    if (not data.next_hop) then return 0 end
-    local next_hop = { x = data.next_hop[1], y = data.next_hop[2] }
-    local messengers = { next_hop }
-    
+    local _, _, _, messengers = messenger_next_waypoint(cfg)
+
+    local avoid_map = AH.get_avoid_map(ai, wml.get_child(cfg, "avoid"), true)
+
     local enemies = AH.get_attackable_enemies()
-    
+
     local base_rating_map = LS.create()
-    local max_rating, best_unit, best_hex = -9e99, nil, nil
+    local max_rating, best_unit, best_hex = - math.huge, nil, nil
     for _,unit in ipairs(escorts) do
         -- Only considering hexes unoccupied by other units is good enough for this
         local reach_map = AH.get_reachable_unocc(unit)
-        
+
         -- Minor rating for the fastest and strongest unit to go first
         local unit_rating = unit.max_moves / 100. + unit.hitpoints / 1000.
-        
+
         reach_map:iter( function(x, y, v)
-            local base_rating = base_rating_map:get(x, y)
-            
-            if (not base_rating) then
-                base_rating = 0
-                
-                -- Distance from messenger is most important; only closest messenger counts for this
-                -- Give somewhat of a bonus for the messenger that has moved the farthest through the waypoints
-                local max_messenger_rating = -9e99
-                for _,m in ipairs(messengers) do
-                    local dist = M.distance_between(x, y, m.x, m.y)
-                    local messenger_rating = 1. / (dist + 2.)
-                    if (dist == 0) then
-                        messenger_rating = 0.20
+            if (not avoid_map:get(x, y)) or ((x == unit.x) and (y == unit.y)) then
+                local base_rating = base_rating_map:get(x, y)
+
+                if (not base_rating) then
+                    base_rating = 0
+
+                    -- Distance from messenger is most important; only closest messenger counts for this
+                    -- Give somewhat of a bonus for the messenger that has moved the farthest through the waypoints
+                    local max_messenger_rating = - math.huge
+                    for _,m in ipairs(messengers) do
+                        local messenger_rating = 1. / (M.distance_between(x, y, m.x, m.y) + 2.)
+                        local wp_rating = MAIUV.get_mai_unit_variables(m, cfg.ai_id, "wp_rating")
+                        messenger_rating = messenger_rating * 10. * (1. + wp_rating * 2.)
+
+                        if (messenger_rating > max_messenger_rating) then
+                            max_messenger_rating = messenger_rating
+                        end
                     end
-                    --local wp_rating = MAIUV.get_mai_unit_variables(m, cfg.ai_id, "wp_rating")
-                    --messenger_rating = messenger_rating * 10. * (1. + wp_rating * 2.)
-                    
-                    if (messenger_rating > max_messenger_rating) then
-                        max_messenger_rating = messenger_rating
+
+                    base_rating = base_rating + max_messenger_rating
+
+                    -- Distance from (sum of) enemies is important too
+                    -- This favors placing escort units between the messenger and close enemies
+                    for _,e in ipairs(enemies) do
+                        base_rating = base_rating + 1. / (M.distance_between(x, y, e.x, e.y) + 2.)
                     end
+
+                    base_rating_map:insert(x, y, base_rating)
                 end
-                
-                base_rating = base_rating + max_messenger_rating
-                
-                -- Distance from (sum of) enemies is important too
-                -- This favors placing escort units between the messenger and close enemies
-                local enemy_rating, count = 0, 0
-                for _,e in ipairs(enemies) do
-                    local e_dist = M.distance_between(next_hop, e)
-                    if (e_dist <= e.max_moves + 1) then
-                        count = count + 1
-                        enemy_rating = enemy_rating + 1. / (M.distance_between(x, y, e) + 2.) / 2.
-                    end
+
+                local rating = base_rating + unit_rating
+
+                if (rating > max_rating) then
+                    max_rating = rating
+                    best_unit, best_hex = unit, { x, y }
                 end
-                if (count > 0) then
-                    enemy_rating = enemy_rating / count
-                    base_rating = base_rating + enemy_rating
-                end
-                
-                base_rating_map:insert(x, y, base_rating)
-            end
-            
-            local rating = base_rating + unit_rating
-            
-            if (rating > max_rating) then
-                max_rating = rating
-                best_unit, best_hex = unit, { x, y }
             end
         end)
     end
-    
+
     -- This will always find at least the hex the unit is on -> no check necessary
     AH.movefull_stopunit(ai, best_unit, best_hex)
 end
--- lua/micro_ais/ca_envoy_move.lua
+++ lua/micro_ais/ca_envoy_move.lua
@@ -1,31 +1,44 @@
-local M = wesnoth.map
-local AH = wesnoth.require "ai/lua/ai_helper"
-local BC = wesnoth.require "ai/lua/battle_calcs"
+local AH = wesnoth.require "ai/lua/ai_helper.lua"
 
 local messenger_next_waypoint = wesnoth.require "./ca_envoy_next_waypoint"
 
 local ca_messenger_move = {}
 
-function ca_messenger_move:evaluation(cfg, data)
+function ca_messenger_move:evaluation(cfg)
     -- Move the messenger toward goal, potentially attack adjacent unit
-    
+
+    local messenger = messenger_next_waypoint(cfg)
+
+    if messenger then return cfg.ca_score end
+    return 0
+end
+
+function ca_messenger_move:execution(cfg)
     local messenger, x, y = messenger_next_waypoint(cfg)
-    if (not messenger) then return 0 end
-    
+
     if (messenger.x ~= x) or (messenger.y ~= y) then
+        local avoid_map = AH.get_avoid_map(ai, wml.get_child(cfg, "avoid"), true)
         local wp = AH.get_closest_location(
             { x, y },
             { { "not", { { "filter", { { "not", { side = wesnoth.current.side } } } } } } },
-            messenger
+            messenger, avoid_map
         )
         x, y = wp[1], wp[2]
     end
-    
-    -- Try to take the "ideal path"
-    local path = AH.find_path_with_shroud(messenger, x, y, { ignore_units = 'yes' })
-    local optimum_hop, optimum_cost = { messenger.x, messenger.y }, 0
-    for _,step in ipairs(path) do
-        local sub_path, sub_cost = AH.find_path_with_shroud(messenger, step[1], step[2])
+
+    local avoid_map = AH.get_avoid_map(ai, wml.get_child(cfg, "avoid"), true)
+
+    local path1 = AH.find_path_with_avoid(messenger, x, y, avoid_map)
+    if (not path1) then path1 = { { messenger.x, messenger.y } } end
+    local next_hop = AH.next_hop(messenger, x, y, { path = path1, avoid_map = avoid_map, ignore_own_units = true } )
+    if (not next_hop) then next_hop = { messenger.x, messenger.y } end
+
+    -- Compare this to the "ideal path"
+    local path2 = AH.find_path_with_avoid(messenger, x, y, avoid_map, { ignore_enemies = true })
+    if (not path2) then path2 = { { messenger.x, messenger.y } } end
+    local optimum_hop = { messenger.x, messenger.y }
+    for _,step in ipairs(path2) do
+        local sub_path, sub_cost = AH.find_path_with_avoid(messenger, step[1], step[2], avoid_map)
         if sub_cost > messenger.moves then
             break
         else
@@ -33,125 +46,98 @@ function ca_messenger_move:evaluation(cfg, data)
             if (not AH.is_visible_unit(wesnoth.current.side, unit_in_way)) then
                 unit_in_way = nil
             end
-            
+
             if unit_in_way and (unit_in_way.side == messenger.side) then
                 local reach = AH.get_reachable_unocc(unit_in_way)
                 if (reach:size() > 1) then unit_in_way = nil end
             end
-            
-            if not unit_in_way then
-                optimum_hop, nh_cost = step, sub_cost
-            end
-        end
-    end
-    
-    local next_hop = optimum_hop
-    
-    data.messenger = messenger
-    data.next_hop = next_hop
-    data.goal = { x, y }
-    
-    if messenger then
-        if (next_hop[1] == x) and (next_hop[2] == y) then
-            return cfg.ca_score
-        else
-            return cfg.ca_score - 10
-        end
-    end
-    return 0
-end
 
-function ca_messenger_move:execution(cfg, data)
-    local messenger = data.messenger
-    local next_hop = data.next_hop
-    local goal = data.goal
-    
-    local go_to
-    if (goal[1] == next_hop[1]) and (goal[2] == next_hop[2]) then
-        go_to = next_hop
-    else
-        local enemies = AH.get_attackable_enemies()
-        local enemy_attack_map = BC.get_attack_map(enemies)
-        
-        go_to = AH.find_best_move(messenger, function(x, y)
-            -- Main rating is distance from goal
-            local rating = -M.distance_between(x, y, goal)
-            
-            -- Minor contribution from terrain, as a tie breaker
-            local hit_chance = messenger:chance_to_be_hit(wesnoth.current.map[{x, y}])
-            rating = rating - hit_chance / 100
-            
-            -- Finally, take enemy threat into account. This is pretty ad hoc so far.
-            local enemy_hp = enemy_attack_map.hitpoints:get(x,y)
-            if enemy_hp then
-               rating = rating - 4 * enemy_hp / messenger.hitpoints
+            if not unit_in_way then
+                optimum_hop = step
             end
-            return rating
-        end, { no_random = true })
+        end
     end
-    
-    if ((go_to[1] ~= messenger.x) or (go_to[2] ~= messenger.y)) then
-        AH.robust_move_and_attack(ai, messenger, go_to)
+
+    -- Now compare how long it would take from the end of both of these options
+    local x_current, y_current = messenger.x, messenger.y
+
+    local unit_in_way = wesnoth.units.get(next_hop[1], next_hop[2])
+    if (unit_in_way == messenger) then unit_in_way = nil end
+    if unit_in_way then unit_in_way:extract() end
+
+    messenger.loc = { next_hop[1], next_hop[2] }
+    local _, cost1 = AH.find_path_with_avoid(messenger, x, y, avoid_map, { ignore_enemies = true })
+
+    local unit_in_way2 = wesnoth.units.get(optimum_hop[1], optimum_hop[2])
+    if (unit_in_way2 == messenger) then unit_in_way2 = nil end
+    if unit_in_way2 then unit_in_way2:extract() end
+
+    messenger.loc = { optimum_hop[1], optimum_hop[2] }
+    local _, cost2 = AH.find_path_with_avoid(messenger, x, y, avoid_map, { ignore_enemies = true })
+
+    messenger.loc = { x_current, y_current }
+    if unit_in_way then unit_in_way:to_map() end
+    if unit_in_way2 then unit_in_way2:to_map() end
+
+    -- If cost2 is significantly less, that means that the optimum path might
+    -- overall be faster even though it is currently blocked
+    if (cost2 + messenger.max_moves/2 < cost1) then next_hop = optimum_hop end
+
+    if next_hop and ((next_hop[1] ~= messenger.x) or (next_hop[2] ~= messenger.y)) then
+        AH.robust_move_and_attack(ai, messenger, next_hop, nil, { avoid_map = avoid_map })
     else
         AH.checked_stopunit_moves(ai, messenger)
     end
-    data.next_hop = nil
-    data.goal = nil
-    
+    if (not messenger) or (not messenger.valid) then return end
+
     -- Test whether an attack without retaliation or with little damage is possible
-    if (messenger.attacks_left <= 0) or (#messenger.attacks == 0) then
-        data.messenger = nil
-        return
-    end
-    
+    if (messenger.attacks_left <= 0) then return end
+    if (#messenger.attacks == 0) then return end
+
     local targets = AH.get_attackable_enemies { { "filter_adjacent", { id = messenger.id } } }
-    
-    local max_rating, best_target, best_weapon = -9e99, nil, nil
+
+    local max_rating, best_target, best_weapon = - math.huge, nil, nil
     for _,target in ipairs(targets) do
         for n_weapon,weapon in ipairs(messenger.attacks) do
             local att_stats, def_stats = wesnoth.simulate_combat(messenger, n_weapon, target)
-            
-            local rating = -9e99
+
+            local rating = - math.huge
             -- This is an acceptable attack if:
             -- 1. There is no counter attack
             -- 2. Probability of death is >=67% for enemy, 0% for attacker (default values)
-            
+
             local enemy_death_chance = cfg.enemy_death_chance or 0.67
             local messenger_death_chance = cfg.messenger_death_chance or 0
-            
+
             if (att_stats.hp_chance[messenger.hitpoints] == 1)
                 or (def_stats.hp_chance[0] >= tonumber(enemy_death_chance)) and (att_stats.hp_chance[0] <= tonumber(messenger_death_chance))
             then
                 rating = target.max_hitpoints + def_stats.hp_chance[0]*100 + att_stats.average_hp - def_stats.average_hp
             end
-            
+
             if (rating > max_rating) then
                 max_rating, best_target, best_weapon = rating, target, n_weapon
             end
         end
     end
-    
+
     if best_target then
         AH.checked_attack(ai, messenger, best_target, best_weapon)
     else
         -- Always attack enemy on last waypoint
-        local waypoint = wesnoth.current.map.special_locations[cfg.waypoint]
-        local waypoint_x, waypoint_y = waypoint[1], waypoint[2]
+        local waypoints = AH.get_multi_named_locs_xy('waypoint', cfg)
         local target = AH.get_attackable_enemies {
-            x = tonumber(waypoint_x),
-            y = tonumber(waypoint_y),
+            x = waypoints[#waypoints][1],
+            y = waypoints[#waypoints][2],
             { "filter_adjacent", { id = messenger.id } }
         }[1]
-        
+
         if target then
             AH.checked_attack(ai, messenger, target)
         end
     end
-    if (not messenger) or (not messenger.valid) then
-        data.messenger = nil
-        return
-    end
-    
+    if (not messenger) or (not messenger.valid) then return end
+
     AH.checked_stopunit_attacks(ai, messenger)
 end
 
