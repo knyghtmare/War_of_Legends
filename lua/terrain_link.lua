6f25587c196f2fd09b27f652a1069b2e2e9411ab
Tahsin Jahin Khalid <5283677+knyghtmare@users.noreply.github.com>
2026-07-28 12:34:51 +0600

    refactor terrain link to use wesnoth.game_events

--- lua/terrain_link.lua
+++ lua/terrain_link.lua
@@ -26,80 +26,84 @@ function wesnoth.wml_actions.activate_terrain_link(cfg)
 end
 ]]
 
-on_event("new turn", function()
-    -- filter for unit with terrain link
-    -- local terrain_linked_units = wesnoth.units.find_on_map { ability = "WOL_terrain_link", side = wesnoth.current.side }
-    local terrain_linked_units = wesnoth.units.find_on_map { ability = "WOL_terrain_link" }
+wesnoth.game_events.add{
+    name = 'new turn',
+    first_time_only = false,
+    action = function()
+        -- filter for unit with terrain link
+        -- local terrain_linked_units = wesnoth.units.find_on_map { ability = "WOL_terrain_link", side = wesnoth.current.side }
+        local terrain_linked_units = wesnoth.units.find_on_map { ability = "WOL_terrain_link" }
 
-    -- set up a for loop
-    for i, terrain_linked_unit in ipairs(terrain_linked_units) do
-        -- take the x and y coordinates of the unit
-        local x_var = terrain_linked_unit.x
-        local y_var = terrain_linked_unit.y
-        -- check for the terrain
-        if wesnoth.map.matches(x_var, y_var, { terrain = "A*^*,Ha*^*,Ms*^*" }) then
-            -- if true, then change variation to appropriate type
-            terrain_linked_unit:add_modification("object", {
-                id="WOL_terrain_link_frost",
-                T.effect{
-                    apply_to = "variation",
-                    name = "frost"
-                }
-            })
-        elseif wesnoth.map.matches(x_var, y_var, { terrain = "S*^*,Chs*^*" }) then
-            -- if true, then change variation to appropriate type
-            terrain_linked_unit:add_modification("object", {
-                id="WOL_terrain_link_swamp",
-                T.effect{
-                    apply_to = "variation",
-                    name = "swamp"
-                }
-            })
-        elseif wesnoth.map.matches(x_var, y_var, { terrain = "W*^*,Chw*^*,Cm*^*,Km*^*" }) then
-            -- if true, then change variation to appropriate type
-            terrain_linked_unit:add_modification("object", {
-                id="WOL_terrain_link_whirlpool",
-                T.effect{
-                    apply_to = "variation",
-                    name = "whirlpool"
-                }
-            })
-        elseif wesnoth.map.matches(x_var, y_var, { terrain = "Ql*^*,Mv*^*,D*^*,Hd*^*,Mdd*^*" }) then
-            -- if true, then change variation to appropriate type
-            terrain_linked_unit:add_modification("object", {
-                id="WOL_terrain_link_sandstorm",
-                T.effect{
-                    apply_to = "variation",
-                    name = "sandstorm"
-                }
-            })
-        elseif wesnoth.map.matches(x_var, y_var, { terrain = "M*^*,Xu*^*,H*^*,Uh*^*,Uu^*,*^Dr" }) then
-            -- if true, then change variation to appropriate type
-            terrain_linked_unit:add_modification("object", {
-                id="WOL_terrain_link_quake",
-                T.effect{
-                    apply_to = "variation",
-                    name = "quake"
-                }
-            })
-        elseif wesnoth.map.matches(x_var, y_var, { terrain = "*^F*,Tb*^*,*^Tf*,*^Gvs, *^Efm" }) then
-            -- if true, then change variation to appropriate type
-            terrain_linked_unit:add_modification("object", {
-                id="WOL_terrain_link_bramble",
-                T.effect{
-                    apply_to = "variation",
-                    name = "bramble"
-                }
-            })
-        else
-            -- for everything else, the base variation
-            terrain_linked_unit:add_modification("object", {
-                id="WOL_terrain_link_base",
-                T.effect{
-                    apply_to = "variation",
-                    name = "tornado"
-                }
-            })
+        -- set up a for loop
+        for i, terrain_linked_unit in ipairs(terrain_linked_units) do
+            -- take the x and y coordinates of the unit
+            local x_var = terrain_linked_unit.x
+            local y_var = terrain_linked_unit.y
+            -- check for the terrain
+            if wesnoth.map.matches(x_var, y_var, { terrain = "A*^*,Ha*^*,Ms*^*" }) then
+                -- if true, then change variation to appropriate type
+                terrain_linked_unit:add_modification("object", {
+                    id="WOL_terrain_link_frost",
+                    T.effect{
+                        apply_to = "variation",
+                        name = "frost"
+                    }
+                })
+            elseif wesnoth.map.matches(x_var, y_var, { terrain = "S*^*,Chs*^*" }) then
+                -- if true, then change variation to appropriate type
+                terrain_linked_unit:add_modification("object", {
+                    id="WOL_terrain_link_swamp",
+                    T.effect{
+                        apply_to = "variation",
+                        name = "swamp"
+                    }
+                })
+            elseif wesnoth.map.matches(x_var, y_var, { terrain = "W*^*,Chw*^*,Cm*^*,Km*^*" }) then
+                -- if true, then change variation to appropriate type
+                terrain_linked_unit:add_modification("object", {
+                    id="WOL_terrain_link_whirlpool",
+                    T.effect{
+                        apply_to = "variation",
+                        name = "whirlpool"
+                    }
+                })
+            elseif wesnoth.map.matches(x_var, y_var, { terrain = "Ql*^*,Mv*^*,D*^*,Hd*^*,Mdd*^*" }) then
+                -- if true, then change variation to appropriate type
+                terrain_linked_unit:add_modification("object", {
+                    id="WOL_terrain_link_sandstorm",
+                    T.effect{
+                        apply_to = "variation",
+                        name = "sandstorm"
+                    }
+                })
+            elseif wesnoth.map.matches(x_var, y_var, { terrain = "M*^*,Xu*^*,H*^*,Uh*^*,Uu^*,*^Dr" }) then
+                -- if true, then change variation to appropriate type
+                terrain_linked_unit:add_modification("object", {
+                    id="WOL_terrain_link_quake",
+                    T.effect{
+                        apply_to = "variation",
+                        name = "quake"
+                    }
+                })
+            elseif wesnoth.map.matches(x_var, y_var, { terrain = "*^F*,Tb*^*,*^Tf*,*^Gvs, *^Efm" }) then
+                -- if true, then change variation to appropriate type
+                terrain_linked_unit:add_modification("object", {
+                    id="WOL_terrain_link_bramble",
+                    T.effect{
+                        apply_to = "variation",
+                        name = "bramble"
+                    }
+                })
+            else
+                -- for everything else, the base variation
+                terrain_linked_unit:add_modification("object", {
+                    id="WOL_terrain_link_base",
+                    T.effect{
+                        apply_to = "variation",
+                        name = "tornado"
+                    }
+                })
+            end
         end
     end
-end)
+}
